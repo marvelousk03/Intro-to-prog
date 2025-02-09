b05227dad75821bfa3a0a74f3df3c69993f1ff61
marvek16 <marvelousk1603f@gmail.com>
2025-02-09 15:20:42 +0200

    commit

---
+++ dummies/chapter5/main.js
@@ -0,0 +1,21 @@
+var myHobbiesHeader = document.getElementById("my-hobbies");
+var item1;
+var item2;
+var item3;
+
+myHobbiesHeader.innerHTML = "What I like to Do";
+
+document.getElementById("my-button").onclick = newList;
+
+function newList() {
+    item1 = prompt("Enter a new first thing.");
+    item2 = prompt("Enter a new second thing.");
+    item3 = prompt("Enter a new third thing.");
+    updateList();
+};
+
+function updateList() {
+    document.getElementById("firstThing").innerHTML = item1;
+    document.getElementById("secondThing").innerHTML = item2;
+    document.getElementById("thirdThing").innerHTML = item3;
+};
---
+++ dummies/chapter7/robots.js
@@ -0,0 +1,6 @@
+document.getElementById("lefteye").style.backgroundColor = "pink";
+document.getElementById("head").style.transform = "rotate(20deg)";
+
+document.getElementById("mouth").style.backgroundColor = "plum"
+
+document.getElementById("rightarm").style.left = "22%"
