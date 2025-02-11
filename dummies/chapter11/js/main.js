3e3bf2942b5d7725d4343718e0f56d860e01ea56
marvek16 <marvelousk1603f@gmail.com>
2025-02-11 11:42:30 +0200

    commit

---
+++ dummies/chapter11/js/main.js
@@ -0,0 +1,18 @@
+var parents = ["father", "mother"];
+var friends = ["Kelly", "Angel"];
+var myPerson = ["Marvelous", 19, true];
+var family = parents.concat(friends, myPerson);
+
+var myDog = "Clover";
+var pet = [myDog];
+
+var array = [1,2,3]
+array[1] = "two";
+
+pet[1] = "Fluffy"
+
+let longArr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+let arr = longArr.concat("Marvelous", true, 15);
+// arr.push(16);
+
+let arr2 = longArr.pop()
