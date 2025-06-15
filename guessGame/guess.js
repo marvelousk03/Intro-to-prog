5a67a765d62f849bdfa2f5cdc2ca8d38aa7d2708
marvek16 <marvelousk1603f@gmail.com>
2025-06-15 19:54:53 +0200

    commit

--- guessGame/guess.js
+++ guessGame/guess.js
@@ -1,8 +1,52 @@
-let secret = Math.floor(Math.random() * 10) + 1;
-let guess;
+// let secret = Math.floor(Math.random() * 10) + 1;
+// let guess;
 
-while ((guess = prompt("Guess a number between 0 and 10")) != secret) {
-    alert(guess < secret ? "Too low!" : "Too high!");
+// while ((guess = prompt("Guess a number between 0 and 10")) != secret) {
+//     alert(guess < secret ? "Too low!" : "Too high!");
+// }
+
+// alert("Congratulations! You got it!");
+
+
+
+// var secNumber = 7;
+
+// while (true) {
+//     let message = Number(prompt("What is the secrect number? Take a guess from 1 to 20."));
+
+//     if (message === secNumber) {
+//         alert("Lucky number 7! You guessed right");
+//         break;
+//     } else {
+//         alert("You seem unlucky today. Try again.")
+//     };
+// };
+
+var secNumber = Math.floor(Math.random() * 10) + 1;
+var theNum = 0;
+console.log(secNumber)
+while (theNum != secNumber) {
+    theNum = prompt("Guess a number from 1 to 10");
+    if (theNum < secNumber) {
+        alert("Too Low!");
+    } else if (theNum > secNumber) {
+        alert("Too High!")
+    } else {
+        break
+    }
 }
+alert("Congratulations! You got it!")
 
-alert("Congratulations! You got it!");
+
+
+// if (number < secret) {
+//     alert("Incorrect, too low");
+// } else if (number > secret) {
+//     alert("Incorrect, too high");
+// } else if (number === null) {
+//     alert(" Please select a number");
+// }
+// else {
+//     break
+// }
+// alert("Congratulations! You got it!")
--- shoppingcart/shop.js
+++ shoppingcart/shop.js
@@ -1,12 +1,82 @@
-let nums = [1, 2, 3, 4, 5, 6];
-let items = ["Coke", "Kit Kat", "Bar One", "Fanta", "Oreo", "Popcorn"];
-let prices = [7.5, 9.5, 8.5, 7.5, 10.0, 6.5];
-let quantities = [0, 0, 0, 0, 0, 0];
-let totals = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0];
-let totalOrderAmt = 0;
+// let nums = [1, 2, 3, 4, 5, 6];
+// let items = ["Coke", "Kit Kat", "Bar One", "Fanta", "Oreo", "Popcorn"];
+// let prices = [7.5, 9.5, 8.5, 7.5, 10.0, 6.5];
+// let quantities = [0, 0, 0, 0, 0, 0];
+// let totals = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0];
+// let totalOrderAmt = 0;
+
+// function add_selection(x) {
+//     console.log(x);
+//     quantities[x] = quantities[x] + 1;
+//     totals[x] = prices[x] * quantities[x];
+//     totalOrderAmt += prices[x];
+
+//     display_all();
+// }
+
+// function remove_selection(x) {
+//     console.log(x);
+//     if (quantities[x] > 0) {
+//         quantities[x] = quantities[x] - 1;
+//         totals[x] = prices[x] * quantities[x];
+//         totalOrderAmt -= prices[x];
+
+//     }
+//     display_all();
+// }
+
+// function total_selection(x) {
+//     alert("Your total is: $" + totalOrderAmt);
+// }
+
+
+// function display_all() {
+
+
+//     let myTable = "<table><th style='width: 100px; color: red; text-align: right;'>Num</th>";
+//     myTable += "<th style='width: 100px; color: red; text-align: right;'>Item</th>";
+//     myTable += "<th style='width: 100px; color: red; text-align: right;'>$Price</th>";
+//     myTable += "<th style='width: 100px; color: red; text-align: right;'>Quantity</th>";
+//     myTable += "<th style='width: 100px; color: red; text-align: right;'>$Total</th>";
+//     myTable += "<th style='width: 100px; color: red; text-align: right;'>Add</th>";
+//     myTable += "<th style='width: 100px; color: red; text-align: right;'>Remove</th>";
+
+//     for (i = 0; i < items.length; i++) {
+//         myTable += "<tr><td style='width: 100px; text-align: right;'>" + nums[i] + "</td>";
+//         myTable += "<td style='width: 100px; text-align: right;'>" + items[i] + "</td><";
+//         myTable += "<td style='width: 100px; text-align: right;'>" + prices[i] + "</td>";
+//         myTable += "<td style='width: 100px; text-align: right;'>" + quantities[i] + "</td>";
+//         myTable += "<td style='width: 100px; text-align: right;'>" + totals[i] + "</td>";
+//         myTable += "<td><button onclick='add_selection(" + i + ")'>Add</button></td>";
+//         myTable += "<td><button onclick='remove_selection(" + i + ")'>Remove</button></td>";
+//     }
+
+//     myTable += "</table>";
+//     myTable += "<br/><br/><p>Total: " + `$` + totalOrderAmt + "</p>";
+//     myTable += "<td><button onclick='total_selection(" + i + ")'>Check Out</button></td>";
+
+
+//     // document.write(myTable);
+//     document.getElementById("demo").innerHTML = myTable;
+
+
+// }
+
+// window.onload = function () {
+//     display_all();
+// }
+
+
+
+var nums = [1, 2, 3, 4];
+var items = ["Coke", "Kit Kat", "Bar One", "Fanta"];
+var prices = [7.5, 9.5, 8.5, 7.5];
+var quantities = [0, 0, 0, 0];
+var totals = [0.0, 0.0, 0.0, 0.0];
+var totalOrderAmt = 0;
 
 function add_selection(x) {
-    console.log(x);
+    console.log("Add:", x);
     quantities[x] = quantities[x] + 1;
     totals[x] = prices[x] * quantities[x];
     totalOrderAmt += prices[x];
@@ -14,15 +84,22 @@ function add_selection(x) {
     display_all();
 }
 
-function remove_selection(x) {
-    console.log(x);
-    if (quantities[x] > 0) {
-        quantities[x] = quantities[x] - 1;
-        totals[x] = prices[x] * quantities[x];
-        totalOrderAmt -= prices[x];
-
+function allToGetHer() {
+    let grandTotal = 0;
+    for (let i = 0; i < totals.length; i++) {
+        grandTotal += totals[i];
     }
-    display_all();
+    return grandTotal;
+}
+
+function remove_selection(x) {
+    console.log("Remove:", x);
+    if (quantities[x] > 0) { 
+        quantities[x] -= 1;
+        totals[x] = prices[x] * quantities[x]; 
+        totalOrderAmt -= prices[x]; 
+    }
+    display_all(); 
 }
 
 function total_selection(x) {
@@ -31,37 +108,48 @@ function total_selection(x) {
 
 
 function display_all() {
-
-
-    let myTable = "<table><th style='width: 100px; color: red; text-align: right;'>Num</th>";
+    // Build the table and include the border attribute only once
+    let myTable = "<table border='1'><tr>";
+    myTable += "<th style='width: 100px; color: red; text-align: right;'>Num</th>";
     myTable += "<th style='width: 100px; color: red; text-align: right;'>Item</th>";
-    myTable += "<th style='width: 100px; color: red; text-align: right;'>$Price</th>";
+    myTable += "<th style='width: 100px; color: red; text-align: right;'>Price</th>";
     myTable += "<th style='width: 100px; color: red; text-align: right;'>Quantity</th>";
-    myTable += "<th style='width: 100px; color: red; text-align: right;'>$Total</th>";
+    myTable += "<th style='width: 100px; color: red; text-align: right;'>Total</th>";
     myTable += "<th style='width: 100px; color: red; text-align: right;'>Add</th>";
     myTable += "<th style='width: 100px; color: red; text-align: right;'>Remove</th>";
+    myTable += "</tr>";
 
-    for (i = 0; i < items.length; i++) {
-        myTable += "<tr><td style='width: 100px; text-align: right;'>" + nums[i] + "</td>";
-        myTable += "<td style='width: 100px; text-align: right;'>" + items[i] + "</td><";
+    // Create table rows for each item
+    for (let i = 0; i < items.length; i++) {
+        myTable += "<tr>";
+        myTable += "<td style='width: 100px; text-align: right;'>" + nums[i] + "</td>";
+        myTable += "<td style='width: 100px; text-align: right;'>" + items[i] + "</td>";
         myTable += "<td style='width: 100px; text-align: right;'>" + prices[i] + "</td>";
         myTable += "<td style='width: 100px; text-align: right;'>" + quantities[i] + "</td>";
         myTable += "<td style='width: 100px; text-align: right;'>" + totals[i] + "</td>";
         myTable += "<td><button onclick='add_selection(" + i + ")'>Add</button></td>";
         myTable += "<td><button onclick='remove_selection(" + i + ")'>Remove</button></td>";
+        myTable += "</tr>";
     }
 
     myTable += "</table>";
-    myTable += "<br/><br/><p>Total: " + `$` + totalOrderAmt + "</p>";
-    myTable += "<td><button onclick='total_selection(" + i + ")'>Check Out</button></td>";
+    // Optionally show the total order amount below the table
+    myTable += "<br/><br/><p>Total Order Amount: " + totalOrderAmt + "</p>";
 
-
-    // document.write(myTable);
+    // Now update the DOM inside the function
     document.getElementById("demo").innerHTML = myTable;
-
-
 }
 
-window.onload = function () {
+// Ensure the Purchase button ID matches your HTML
+var button = document.getElementById("Shopright");
+button.addEventListener("click", TotalForEverything);
+
+function TotalForEverything() {
+    let finalAmount = allToGetHer();
+    alert("Total amount to pay: $" + finalAmount);
+}
+
+// Make sure the table is displayed when the page loads
+window.onload = function() {
     display_all();
-}
+};
